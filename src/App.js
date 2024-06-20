b68e1b6e1e60914d680861b49049ac7ca8ae6bda
chyeon97 <pcyeon07@gmail.com>
2024-06-20 16:29:12 +0900

    test: firebase db 연결 및 테스트 소스

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import SearchInput from "./components/SearchInput";
 import Card from "./components/Card";
+import Header from './components/Header';
+import {useEffect} from 'react';
+import {getBookMarkList} from './firebase';
 
 let test = Array.from({length: 20}, () => { return {url: "www.naver.com", content: "네이버"}})
 
 function App() {
+    useEffect(() => {
+        (async () => {
+            const res = await getBookMarkList("book-mark")
+            console.log(res)
+        })();
+    }, [])
+
   return (
     <>
-     <SearchInput/>
+        <Header/>
+        <SearchInput/>
 
-      
         <ul className="wrap">
           {test.map((item, index) => {
             return <Card key={index} data={item}/>
