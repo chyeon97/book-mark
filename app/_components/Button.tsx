dadd04e9e2dcab3d57f09f0e96dba412cb3f3c03
chyeon97 <pcyeon07@gmail.com>
2024-08-07 22:03:30 +0900

    style: 반응형 햄버거 버튼 추가

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 interface ButtonProps {
+    sm_show?: boolean,
     icon?:string,
     text?:string,
     iconStyle: {width: number, height?: number, hoverStyle?: {default: string, dark: string}},
     textStyle?: {color: string, size: string}, // TODO 폰트사이즈, 색상 -> 나중에 tailwind 형식으로 변경하기 
 }
 
-const Button = ({icon, text, iconStyle, textStyle} : ButtonProps) => {
+const Button = ({sm_show, icon, text, iconStyle, textStyle} : ButtonProps) => {
     
     return (
-        <button className={`flex box-border border border-none rounded-md py-3 px-3 bg-gray-100 hover:${iconStyle?.hoverStyle?.default}`}>
+        <button className={`${sm_show && "visible sm:invisible"} flex box-border border border-none rounded-md py-3 px-3 bg-gray-100 hover:${iconStyle?.hoverStyle?.default}`}>
             {icon && <img width={iconStyle ? `${iconStyle.width}` : "50"} height="50" src={icon} alt="아이콘"/>}
             {text && <p style={textStyle ? {...textStyle} : {}}>{text}</p>}
         </button>
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,16 +2,17 @@ import React, { ReactElement } from "react";
 import Button from "./Button";
 
 const buttonList = [
-    { icon: "https://img.icons8.com/ios/50/do-not-disturb-2.png", width: 35, hoverStyle: { default: "bg-gray-200", dark: "bg-white" } },
+    { sm_show: true, icon: "https://img.icons8.com/ios-filled/50/menu--v1.png", width: 30,},
+    { sm_show: false, icon: "https://img.icons8.com/ios/50/do-not-disturb-2.png", width: 30, hoverStyle: { default: "bg-gray-200", dark: "bg-white" } },
 ]
 
 
 const Header = () => {
     return (
-        <div className="flex container h-20 justify-end items-center px-3">
+        <div className="flex container h-20 justify-between items-center px-3">
             {buttonList.map((info) => {
                 return (
-                    <Button icon={info.icon} iconStyle={{ width: info.width, hoverStyle: info.hoverStyle}} />
+                    <Button sm_show={info.sm_show} icon={info.icon} iconStyle={{ width: info.width, hoverStyle: info.hoverStyle}} />
                 )
             })}
         </div>
