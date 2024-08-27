af6db889f11e70badc62f9fe94aba7764ce4a7d7
chyeon97 <pcyeon07@gmail.com>
2024-08-27 14:55:30 +0900

    style: 대시보드 모바일 화면 개발

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { Card, SearchInput } from "@/components";
 const DashBoard = () => {
 
   return (
-    <div className="space-y-4">
-        <div className="flexRowMode space-x-4">
+    <div className="flex flex-col w-full space-y-4 sm:space-y-4">
+        <div className="hidden sm:flexRowMode sm:space_between space-x-4">
           <Card width="50%" bgColor="black" minHeight="1rem" padding="10">
             <h2>북마크 등록</h2>
             <p className="text-4xl text-right">⇀</p> 
@@ -15,6 +15,16 @@ const DashBoard = () => {
           <Card width="50%" bgColor="white" minHeight="1rem" padding="10">
             오늘 알람
           </Card>
+        </div>
+
+        <div className="space-y-4 mt-2 sm:hidden sm:space-y-0">
+          <Card width="100%" bgColor="black" minHeight="1rem" padding="10">
+              <h2>북마크 등록</h2>
+            </Card>
+
+            <Card width="100%" bgColor="white" minHeight="1rem" padding="10">
+              오늘 알람
+            </Card>
         </div>
         
         <Card width="100%" bgColor="white" minHeight="2rem" padding="10">
